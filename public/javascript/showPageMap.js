4563a86560e1c9577266d96825e16cd5e2379672
Heather OBrien <haobrientx@gmail.com>
2021-08-03 12:26:26 -0500

    Style changes

--- public/javascript/showPageMap.js
+++ public/javascript/showPageMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    container: 'show-map', // container ID
+    style: 'mapbox://styles/mapbox/outdoors-v11', // style URL
     center: camp.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
+
+map.addControl(new mapboxgl.NavigationControl())
 
 new mapboxgl.Marker()
     .setLngLat(camp.geometry.coordinates)
